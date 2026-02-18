6908d1ab825bab169ec1cce35e1f88a71f9a3a56
Valtteri Markus Ahde <vmahde13@gmail.com>
2026-02-18 16:17:41 +0200

    Muokattu lomaketta

--- laheta.php
+++ laheta.php
@@ -1,5 +1,27 @@
 <?php
 require 'db.php';
 
+# Luetaan lomakkeen tiedot.
+$nimi = isset($_POST["nimi"]) ? $_POST["nimi"] : "";
+$sahkoposti = isset($_POST["sahkoposti"]) ? $_POST["sahkoposti"] : "";
+$viesti = isset($_POST["viesti"]) ? $_POST["viesti"] : "";
 
+# Jos joku tiedoista puuttuu -> Takaisin lomakkeelle
+if (empty($nimi) || empty($sahkoposti) || empty($viesti)) {
+    header("Location: yhteystiedot.html");
+}
+
+# SQL-lause
+$sql = "INSERT INTO yhteydenotto (nimi, sahkoposti, viesti) VALUES (?, ?, ?)";
+
+$stmt = mysqli_prepare($yhteys, $sql);
+
+mysqli_stmt_bind_param($stmt, 'sss', $nimi, $sahkoposti, $viesti);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_close($yhteys);
+
+header("Location: kiitos.html");
+exit;
 ?>
