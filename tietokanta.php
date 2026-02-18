875c39127143fa9f25ce6ac729e7f7032659e2bc
Valtteri Markus Ahde <vmahde13@gmail.com>
2026-02-18 15:19:21 +0200

    Lisätty tietokanta.php

---
+++ tietokanta.php
@@ -0,0 +1,10 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    $yhteys = mysqli_connect("db", "root", "password", "HAMKpizzakebab");
+} catch (Exception $e) {
+    header("Location: yhteysvirhe.html")
+    exit;
+}
+?>
